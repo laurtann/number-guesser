d8eda71a7839fbc92b5b6ee500359c42e0f60530
Laura Tannahill <lntannahill@gmail.com>
2021-02-04 19:39:13 -0400

    clean up comments

--- Program.cs
+++ Program.cs
@@ -1,69 +1,62 @@
 ﻿using System;
 
-// Namespace
 namespace NumberGuesser
     
 {
-    // Main Class
     class Program
     {
-        // Entry Point Method - void means that it isn't going to return a val
-        // Static - no instances refers to func itself
         static void Main(string[] args)
         {
-            GetAppInfo(); // run func for app info
+            GetAppInfo(); // Run func to print app info
 
-            GreetUser(); // Ask user name & greet
+            GreetUser(); // Ask user for name & greet
 
             while (true) {
 
-                // init correct number
-                // int correctNumber = 7;
-
-                // create random object
+                // Create random object
                 Random random = new Random();
 
+                // Random number btw 1 and 10
                 int correctNumber = random.Next(1, 11);
 
-                // init guess var
+                // Set initial guess to 0
                 int guess = 0;
 
-                // ask user for number
+                // Prompt user for guess
                 Console.WriteLine("Guess a number between 1 and 10");
 
                 while(guess != correctNumber)
                 {
-                    // get user input
+                    // Get user input
                     string userInput = Console.ReadLine();
 
-                    // make sure input is number
+                    // Check that user input is an int
                     if (!int.TryParse(userInput, out guess))
                     {
-                        // print error msg
+                        // Print error message
                         PrintColorMessage(ConsoleColor.Red, "Please enter a number");
 
-                        //keep going
                         continue;
                     }
 
-                    // cast to int and put into guess var
+                    // Cast user input to int and put into guess var
                     guess = Int32.Parse(userInput);
 
-                    // match guess to correct num
+                    // Match guess to correct num
                     if (guess != correctNumber)
                     {
-                        // print error msg
+                        // Print error msg
                         PrintColorMessage(ConsoleColor.Red, "Wrong number, please try again");
                     }
                 }
 
-                // output success msg
+                // Output success msg
                 PrintColorMessage(ConsoleColor.Green, "Correct!");
 
-                // ask to play again
+                // Ask user to play again
                 Console.WriteLine("Play again? (Y or N)");
 
-                // get answer
+                // Get user answer
                 string answer = Console.ReadLine().ToUpper();
 
                 if (answer == "Y") {
@@ -76,42 +69,39 @@ namespace NumberGuesser
 
         }
 
-        // get and display app info
+        // Display app info
         static void GetAppInfo() {
             // set app vars
             string appName = "Number Guesser";
             string appVersion = "1.0.0";
             string appAuthor = "laurtann";
 
-            // change text clr
+            // Change text clr
             Console.ForegroundColor = ConsoleColor.Green;
 
-            // app info
+            // App info
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
 
-            // reset clr - makes the rest of the text white again
             Console.ResetColor();
         }
 
-        // get user name and make greeting
+        // Get user's name and make greeting
         static void GreetUser() {
-            // ask user for name
             Console.WriteLine("What is your name?");
 
-            // get user input
+            // Get user input
             string userName = Console.ReadLine();
 
             Console.WriteLine("Hello {0}, let's play a game...", userName);
         }
 
         static void PrintColorMessage(ConsoleColor color, string message) {
-            // change text clr
+            // Change text clr
             Console.ForegroundColor = color;
 
-            // tell user wrong input type
+            // Error or success msg
             Console.WriteLine(message);
 
-            // reset clr - makes the rest of the text white again
             Console.ResetColor();
         }
     }
