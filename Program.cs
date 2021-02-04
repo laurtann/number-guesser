c22fcd464e0a9dab5c1398d17cb24df2673b747b
Laura Tannahill <lntannahill@gmail.com>
2021-02-04 16:50:37 -0400

    Add header

--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
+// Namespace
 namespace NumberGuesser
+    
 {
+    // Main Class
     class Program
     {
+        // Entry Point Method - void means that it isn't going to return a val
+        // Static - no instances refers to func itself
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // set app vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "laurtann";
+
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
         }
     }
 }
