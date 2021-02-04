57cde92ea94cc4e5dd8fe5a1aa734cb5b31b1afa
Laura Tannahill <lntannahill@gmail.com>
2021-02-04 17:09:23 -0400

    Begin adding game logic for number guesser

--- Program.cs
+++ Program.cs
@@ -16,7 +16,38 @@ namespace NumberGuesser
             string appVersion = "1.0.0";
             string appAuthor = "laurtann";
 
+            // change text clr
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // app info
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+            // reset clr - makes the rest of the text white again
+            Console.ResetColor();
+
+            // ask user for name
+            Console.WriteLine("What is your name?");
+            
+            // get user input
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, let's play a game...", input);
+
+            // init correct number
+            int correctNumber = 7;
+
+            // init guess var
+            int guess = 0;
+
+            // ask user for number
+            Console.WriteLine("Guess a number between 1 and 10");
+
+            while(guess != correctNumber)
+            {
+                Console.WriteLine("Guess a number between 1 and 10");
+            }
+
         }
     }
 }
+
